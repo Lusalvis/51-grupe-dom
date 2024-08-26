992d814dc0303681fb2c7a92ce944f3b19f900f9
Lusalvis <salvislukas@gmail.com>
2024-08-26 16:40:41 +0300

    wtf

--- js/basketball.js
+++ js/basketball.js
@@ -9,8 +9,8 @@ let quarter = 1;
 const quartersScores = [];
 
 function updateScoreBoard() {
-    document.getElementById('home-score').textContent = homeScore;
-    document.getElementById('guest-score').textContent = guestScore;
+    document.getElementById('homeScore').textContent = homeScore;
+    document.getElementById('guestScore').textContent = guestScore;
     document.getElementById('quarter').textContent = 'Kelinys: ${quarter}';
 }
 
@@ -30,7 +30,21 @@ function nextQuarter() {
         home: homeScore,
         guest: guestScore,
     });
+ 
+    const scoreTable =document.getElementById('scores-table');
+    const row = scoresTable.insertRow();
+    const cell1 = row.insertCell(0);
+    const cell2 = row.insertCell(1);
+    const cell3 = row.insertCell(2);
 
+    cell1.textContetnt ='Kelinys ${quarter}';
+    cell2.textContent = homeScore;
+    cell3.textContent = guestScore;
+
+    quarter++;
+    homeScore = 0;
+    guestScore = 0;
+    updateScoreBoard();
 
 }
 
@@ -43,3 +57,8 @@ document.getElementById('home-3pt').addEventListener('click', () => addHomePoint
 document.getElementById('guest-1pt').addEventListener('click', () => addGuestPoints(1));
 document.getElementById('guest-2pt').addEventListener('click', () => addGuestPoints(2));
 document.getElementById('guest-3pt').addEventListener('click', () => addGuestPoints(3));
+
+document.getElementById('next-quarter').addEventListener('click', () => nextQuarter);
+
+
+updateScoreBoard();
